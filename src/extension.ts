f0239c73b29adb0f94a62012a3da89d3effb1a01
mayank2424 <mayankgupta2424@gmail.com>
2024-12-24 13:05:00 +0530

    Updated logic to append the packages and src path + attched URL to UI

--- src/extension.ts
+++ src/extension.ts
@@ -40,31 +40,38 @@ export function activate(context: vscode.ExtensionContext) {
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Link To Source Code 2!');
 	});
-	
+
 	context.subscriptions.push(selectionListener, disposable);
+		
+
 	context.subscriptions.push(vscode.languages.registerHoverProvider(
-		'typescript',
+		['javascript', 'typescript'],
 		{
 			provideHover(document, position, token) {
+				let contentMessage = `Loading source code URL...`;
+
 				if(!isCtrlOrCommandPressed as boolean) {
 					return;
 				};
 
-				console.log({ document, position, token });
 				const wordRange = document.getWordRangeAtPosition(position, /['"`][^'"`]+['"`]/);
                 if (!wordRange) {
 					return;
 				};
 
 				const hoveredContent = document.getText(wordRange);
-				console.log({ hoveredContent});
 
 				if(hoveredContent) {
 					const cleanedContent = hoveredContent.replace(/^['"]|['"]$/g, "");
-					resolveSourceUrl(cleanedContent);
-				}
+					const URL = resolveSourceUrl(cleanedContent);
 
-				return new vscode.Hover("Navigate to source code");
+					if(URL) {
+						// Remove loading hover pushed initially
+						contentMessage = `[View Source](${URL}): ${URL}`; 
+
+						return new vscode.Hover(contentMessage);
+					}
+				}
 			}
 		}
 	));
@@ -74,39 +81,22 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() {}
 
 
-function resolveSourceUrl(moduleName: string): string | null {
+function resolveSourceUrl(moduleName: string): string | undefined | null {
 	console.log({ moduleName });
     if (moduleName.startsWith('@') || !moduleName.startsWith('.')) {
 		 // Resolve to a CDN or repository
 		// Open Github repository
-		getMatchingPackageNameUsingPath(moduleName);
+		const packageUrl = getMatchingPackageNameUsingPath(moduleName);
 		// getModuleRepositoryURL(moduleName);
-		return `https://unpkg.com/${moduleName}`;
+		return packageUrl;
         
-    } else {
-       // Local import, resolve to file path
-	   return null; // Implement relative path handling if required
-    }
+    };
+	
+	return null;
 }
 
-function getModuleRepositoryURL(moduleName: string) {
-	console.log({ moduleName })
-	try {
-		const packagePath = require.resolve(path.join(`node_modules/${moduleName}`, 'package.json'));
-		console.log({ packagePath });
-
-		// Read 
-		
-		return JSON.parse(fs .readFileSync(packagePath, 'utf8'));
-	} catch(error) {
-		console.error(`Could not find package.json for ${moduleName}:`, error);
-        return null;
-	}
-}
-
-function getMatchingPackageNameUsingPath(pathName: string) {
+function getMatchingPackageNameUsingPath(pathName: string, language = 'typescript') {
 	const splittedPath = pathName.split('/');
-	console.log({ splittedPath });
 
 	// check iteratively if the package name exists in the source code
 	let packageName = null;
@@ -116,6 +106,8 @@ function getMatchingPackageNameUsingPath(pathName: string) {
 		vscode.window.showInformationMessage('Could not find package.json for the module');
 		return;
 	}
+
+	console.log({ splittedPath });
 	
 	for (let i = splittedPath.length; i >= 0; i--) {
 		try {
@@ -129,20 +121,17 @@ function getMatchingPackageNameUsingPath(pathName: string) {
 				const packagePathInNodeModulesDirectory = path.join(projectNodeModulesDirectory, currentPath);
 
 				const packagePath = require.resolve(path.join(packagePathInNodeModulesDirectory, 'package.json'));
-				console.log({ packagePath });
+				console.log({ packagePath, i, a: splittedPath.slice(i) });
 
 				// Find remaining paths from the splitted path
-				const remainingPath = splittedPath.slice(i).join('/');
-		
+				const remainingPath = identifyRemainingPathFileTypeOrDirectory(packagePathInNodeModulesDirectory, splittedPath.slice(i).join('/'));
+				console.log("Remaining pth", { remainingPath });
+
 				if (packagePath) {
-					const modulePackageJson = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
-					
 					// Extract repository URL
-					const repositoryURL = getRepositoryUrlFromPackageJson(packagePath);
-					
-					// TODO: Add logic to open the repository URL and navigate to the file
-					
-					
+					let repositoryURL = generateRepositoryUrl(packagePath, packagePathInNodeModulesDirectory, remainingPath);
+					return repositoryURL;
+
 				}
 
 				break;
@@ -152,8 +141,6 @@ function getMatchingPackageNameUsingPath(pathName: string) {
 			continue;
 		}
 	}
-	
-	
 }
 
 function findProjectNodeModulesDirectoryPath() {
@@ -194,15 +181,146 @@ function checkIfModuleExistsInSourcePackageJson(moduleName: string): boolean {
 	return false;
 }
 
-function getRepositoryUrlFromPackageJson(packagePath: string) {
+function identifyRemainingPathFileTypeOrDirectory(packageNodeModulesBasePath: string, remainingPath: string) {
+	if(!remainingPath) {
+		return '';
+	}
+
+	const fullPath = path.join(packageNodeModulesBasePath, remainingPath);
+	const supportedLanguageExtensions = [
+		".js",
+		".ts",
+		".d.ts",
+		".jsx",
+		".tsx",
+		".json",
+		".css",
+		".scss",
+		".less",
+		".sass",
+		".md",
+		".html",
+		".vue",
+		".svelte",
+		".php",
+		".py",
+		".rb",
+	]
+
+	if(fs.existsSync(fullPath) && fs.lstatSync(fullPath).isDirectory()) {
+		return `/tree/master/${remainingPath}`;
+	}
+	else if(!fs.existsSync(fullPath)) {
+		for(const extension of supportedLanguageExtensions) {
+			const fileWithExtension = fullPath + extension;
+			if(fs.existsSync(fileWithExtension)) {
+				console.log({ fileWithExtension });
+				return `/blob/master/${remainingPath}${extension}`;
+			}
+		}
+	} else {
+		throw new Error('File type not supported');
+	}
+}
+
+function generateRepositoryUrl(packagePath: string, packagePathInNodeModulesDirectory: string, remainingPath?: string) {
 	const modulePackageJson = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
 
 	// Extract repository URL
+	const repositoryData = modulePackageJson.repository;
+	const repositoryUrl = getSourceCodeRepositoryBaseUrl(modulePackageJson, packagePathInNodeModulesDirectory);
+	const repositoryPackage = repositoryData.package || repositoryData.directory;
+	const packageName = modulePackageJson.name;
+
+	console.log({ repositoryPackage, remainingPath, packagePath, packagePathInNodeModulesDirectory });
+
+	let repoUrl;
+
+	const srcPath = path.join(packagePathInNodeModulesDirectory, "src");
+	const hasRemainingPathSrc = remainingPath?.includes('/src');
+	const hasSrcDirectory = !hasRemainingPathSrc && fs.existsSync(srcPath);
+	const hasPackages = !repositoryPackage && isScopedPackage(packageName);
+
+	// If remaining path already contains /tree/master or /blob/master, then append the remaining path to the repository URL
+	if(remainingPath?.includes('/tree/master')) {
+		const updatedRemainingPath = remainingPath.replace('/tree/master', '');
+		let updatedFullPath = `${repositoryUrl}/tree/master${repositoryPackage ? `/${repositoryPackage}` : ''}`;
+
+		if(hasSrcDirectory) {
+			updatedFullPath+= '/src';
+		} else if(hasPackages) {
+			updatedFullPath+= '/packages';
+		}
+
+		repoUrl = `${updatedFullPath}${updatedRemainingPath}`;
+		
+	} else if(remainingPath?.includes('/blob/master')) {
+		const updatedRemainingPath = remainingPath.replace('/blob/master', '');
+		let updatedFullPath = `${repositoryUrl}/blob/master${repositoryPackage ? `/${repositoryPackage}` : ''}`;
+
+		if(hasSrcDirectory) {
+			updatedFullPath+= '/src';
+		} else if(hasPackages) {
+			updatedFullPath+= '/packages';
+		}
+
+		repoUrl = `${updatedFullPath}${updatedRemainingPath}`;
+	} else {
+		// if git repository is package or directory, then /tree/master is appended
+		// because it is assumed that the repository is a directory
+		if(hasSrcDirectory) {	
+			repoUrl = `${repositoryUrl}${repositoryPackage ? '/tree/master/src/' + repositoryPackage : ''}`;
+		} 
+		else if(hasPackages) {
+			repoUrl = `${repositoryUrl}${repositoryPackage ? '/tree/master/packages/' + repositoryPackage : ''}`;
+		}
+		else {
+			repoUrl = `${repositoryUrl}${repositoryPackage ? '/tree/master/' + repositoryPackage : ''}`;
+		}
+	}
+
+	return repoUrl;
+}
+
+function isScopedPackage(packageName: string) {
+	return packageName.startsWith('@') && packageName.includes('/');
+}
+
+
+function getSourceCodeRepositoryBaseUrl(modulePackageJson: any, packagePathInNodeModulesDirectory: string) {
 	const repository = modulePackageJson.repository;
-	const repositoryUrl = repository.url;
-	const repositoryPackage = repository.package;
+	let url;
 
-	// Check if repository URL is of
+	if(typeof repository === "string") {
+		url = repository;
+	} else if(repository?.url) {
+		url = repository.url;
+	}
 
-	return `${repositoryUrl}${repositoryPackage ? '/' + repositoryPackage : ''}`;
+	const providers = {
+		github: "https://github.com",
+		gitlab: "https://gitlab.com",
+		bitbucket: "https://bitbucket.org",
+	  };
+
+	const [provider, repoPath] = url.split(":");	
+	const matchedProvider = providers[provider as keyof typeof providers]
+	if(matchedProvider) {
+		url = `${matchedProvider}/${repoPath}`;
+	}
+
+	// Replace git+ssh with https
+	if (url?.includes('git+ssh')) {	
+		url = url.replace('git+ssh://git@', 'https://');
+	} else if(url?.includes("git://")) {
+		url = url.replace('git://', 'https://');
+	}
+
+	url = url.replace(/\.git$/, '');
+
+	return url;
+}
+
+function determineExtensionBasedOnLanguage() {
+
 }
